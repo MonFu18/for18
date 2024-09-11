f6a6540fcd39f695861899e5832b5cc96b7f1cda
MonFu18 <montidafu@gmail.com>
2024-09-11 12:28:50 +0700

    Initial Commit

---
+++ for18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  for18
+//
+//  Created by Sea Ray on 11/9/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
